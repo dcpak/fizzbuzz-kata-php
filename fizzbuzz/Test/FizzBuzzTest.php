cd85e3e6b1d459242da5bae19bc4775dac6a69ea
Krzysztof Jelski <krzysztof.jelski@pragmatists.pl>
2016-06-20 06:50:03 +0200

    Fizzbuzz starter

---
+++ fizzbuzz/Test/FizzBuzzTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace fizbuzz\Test;
+
+class FizzBuzzTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function test___START_HERE() {
+        $this->assertEquals(5, 2+2);
+    }
+
+}
